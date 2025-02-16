571c6606fd647270ef39fefaab8572268c32ce9e
RobenusW <robenuswork@gmail.com>
2025-02-16 00:41:31 -0500

    trying google

--- src/Account/AuthScreen.tsx
+++ src/Account/AuthScreen.tsx
@@ -85,7 +85,7 @@ export default function AuthScreen() {
                 },
               },
             }}
-            providers={[]}
+            providers={['google']}
           />
         </div>
       </div>
