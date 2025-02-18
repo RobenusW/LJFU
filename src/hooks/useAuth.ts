347154a4f9740a9d8c7b625b61cff3cab2fc83f1
RobenusW <robenuswork@gmail.com>
2025-02-18 11:35:48 -0500

    add g

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,5 +3,9 @@ import { AuthContext } from "../CreateContext.tsx";
 import { User } from "@supabase/supabase-js";
 
 export function useAuth(): { user: User | null } {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+  if (context === null) {
+    throw new Error("Context must be defined");
+  }
+  return context;
 }
