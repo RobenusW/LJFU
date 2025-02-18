085799f21e2cf75c320927a1e60d5cadbd9580b2
RobenusW <robenuswork@gmail.com>
2025-02-18 11:07:27 -0500

    r g

--- src/Account/AuthScreen.tsx
+++ src/Account/AuthScreen.tsx
@@ -54,7 +54,7 @@ export default function AuthScreen() {
                 },
               },
             }}
-            providers={["google"]}
+            providers={[]}
           />
         </div>
       </div>
